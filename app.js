dc4bb2249a8e18513ec6e66f25c8b005c2186767
Ikenna <ikennaagada@gmail.com>
2022-03-16 13:02:13 +0000

    added a data.js file containing the array for the API

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const data = require('./data')
 
 app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send("LAP 1 Code Challenge")
---
+++ data.js
@@ -0,0 +1,19 @@
+const basketballTeams = [
+    {id: 1, team: "Los Angeles Laker", link: "https://www.nba.com/lakers/"},
+    {id: 2, team: "Phoenix Suns", link: "https://www.nba.com/suns/"},
+    {id: 3, team: "Golden State Warriors", link: "https://www.nba.com/warriors/"},
+    {id: 4, team: "Houston Rockets", link: "https://www.nba.com/rockets/"},
+    {id: 5, team: "Los Angeles Clippers", link: "https://www.nba.com/clippers/"},
+    {id: 6, team: "Utah Jazz", link: "https://www.nba.com/jazz/"},
+    {id: 7, team: "Portland Trail Blazers", link: "https://www.nba.com/blazers/"},
+    {id: 8, team: "Denver Nuggets", link: "https://www.nba.com/nuggets/"},
+    {id: 9, team: "San Antonio Spurs", link: "https://www.nba.com/spurs/"},
+    {id: 10, team: "Dallas Mavericks", link: "https://www.mavs.com/"},
+    {id: 11, team: "Memphis Grizzlies", link: "https://www.nba.com/grizzlies/"},
+    {id: 12, team: "Sacramento Kings", link: "https://www.nba.com/kings/"},
+    {id: 13, team: "Oklahoma City Thunder", link: "https://www.nba.com/thunder/"},
+    {id: 14, team: "New Orleans Pelicans", link: "https://www.nba.com/pelicans/"},
+    {id: 15, team: "Minnesota Timberwolves", link: "https://www.nba.com/timberwolves/"}
+]
+
+module.exports = basketballTeams;
