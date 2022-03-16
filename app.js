fda053f5aeaf692f58c57f94771534a879fc78ab
Ikenna <ikennaagada@gmail.com>
2022-03-16 14:46:40 +0000

    Added team selection and random team generator

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const data = require('./data')
 app.use(express.json())
 
 
+function getRandomTeam () {
+    let teamSelector = Math.floor(Math.random() * 14)
+    return teamSelector;
+  }
+
 app.get('/', (req, res) => {
-    res.send("LAP 1 Code Challenge")
+    res.json(data)
 })
+
+app.get("/random", (req, res) => {
+    res.send(data[getRandomTeam()])
+})
+
+
+app.get("/:team", (req, res) => {
+    const idx = req.params.team.toLowerCase();
+
+    data.map( teamData => {
+        const currTeam = teamData.team.toLowerCase()
+        if ( currTeam.includes(idx) ) {
+            res.send(teamData);
+        }
+    })
+})
+
 
 
 
--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const basketballTeams = [
-    {id: 1, team: "Los Angeles Laker", link: "https://www.nba.com/lakers/"},
-    {id: 2, team: "Phoenix Suns", link: "https://www.nba.com/suns/"},
-    {id: 3, team: "Golden State Warriors", link: "https://www.nba.com/warriors/"},
-    {id: 4, team: "Houston Rockets", link: "https://www.nba.com/rockets/"},
-    {id: 5, team: "Los Angeles Clippers", link: "https://www.nba.com/clippers/"},
-    {id: 6, team: "Utah Jazz", link: "https://www.nba.com/jazz/"},
-    {id: 7, team: "Portland Trail Blazers", link: "https://www.nba.com/blazers/"},
-    {id: 8, team: "Denver Nuggets", link: "https://www.nba.com/nuggets/"},
-    {id: 9, team: "San Antonio Spurs", link: "https://www.nba.com/spurs/"},
-    {id: 10, team: "Dallas Mavericks", link: "https://www.mavs.com/"},
-    {id: 11, team: "Memphis Grizzlies", link: "https://www.nba.com/grizzlies/"},
-    {id: 12, team: "Sacramento Kings", link: "https://www.nba.com/kings/"},
-    {id: 13, team: "Oklahoma City Thunder", link: "https://www.nba.com/thunder/"},
-    {id: 14, team: "New Orleans Pelicans", link: "https://www.nba.com/pelicans/"},
-    {id: 15, team: "Minnesota Timberwolves", link: "https://www.nba.com/timberwolves/"}
+    {position: 9, team: "Los Angeles Lakers", link: "https://www.nba.com/lakers/"},
+    {position: 1, team: "Phoenix Suns", link: "https://www.nba.com/suns/"},
+    {position: 3, team: "Golden State Warriors", link: "https://www.nba.com/warriors/"},
+    {position: 15, team: "Houston Rockets", link: "https://www.nba.com/rockets/"},
+    {position: 8, team: "Los Angeles Clippers", link: "https://www.nba.com/clippers/"},
+    {position: 4, team: "Utah Jazz", link: "https://www.nba.com/jazz/"},
+    {position: 11, team: "Portland Trail Blazers", link: "https://www.nba.com/blazers/"},
+    {position: 6, team: "Denver Nuggets", link: "https://www.nba.com/nuggets/"},
+    {position: 12, team: "San Antonio Spurs", link: "https://www.nba.com/spurs/"},
+    {position: 5, team: "Dallas Mavericks", link: "https://www.mavs.com/"},
+    {position: 2, team: "Memphis Grizzlies", link: "https://www.nba.com/grizzlies/"},
+    {position: 13, team: "Sacramento Kings", link: "https://www.nba.com/kings/"},
+    {position: 14, team: "Oklahoma City Thunder", link: "https://www.nba.com/thunder/"},
+    {position: 10, team: "New Orleans Pelicans", link: "https://www.nba.com/pelicans/"},
+    {position: 7, team: "Minnesota Timberwolves", link: "https://www.nba.com/timberwolves/"}
 ]
 
 module.exports = basketballTeams;
